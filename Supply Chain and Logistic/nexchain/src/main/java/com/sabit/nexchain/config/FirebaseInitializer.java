738aba9b1e3b45acae0de7d5a9e3e8f3842054ee
sabithassann <61846421+sabithassann@users.noreply.github.com>
2024-01-15 02:15:24 +0600

    add
    add

---
+++ Supply Chain and Logistic/nexchain/src/main/java/com/sabit/nexchain/config/FirebaseInitializer.java
@@ -0,0 +1,29 @@
+package com.sabit.nexchain.config;
+
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Component;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Component
+public class FirebaseInitializer {
+
+    @PostConstruct
+
+    public void initialize() throws IOException {
+        FileInputStream serviceAccount = new FileInputStream("src/main/resources/firebase/serviceAccountKey.json");
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        FirebaseApp.initializeApp(options);
+
+        
+    }
+}
