5c2a04728f1f8d292690f252ca38d9319aa67ea5
sabithassann <61846421+sabithassann@users.noreply.github.com>
2024-01-16 08:34:19 +0600

    add
    add

---
+++ Home/Supply Chain and Logistic/nexchain/src/main/java/com/sabit/nexchain/repository/RawMaterialRepository.java
@@ -0,0 +1,7 @@
+package com.sabit.nexchain.repository;
+
+import com.sabit.nexchain.model.RawMaterial;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RawMaterialRepository extends JpaRepository<RawMaterial, Long> {
+}
